15559483504b472c2db5eca2444c62bbb6d5c9b1
Maycon RR Alves <mayconrralves@yahoo.com.br>
2020-04-07 16:13:08 -0300

    remove console logs

--- src/store/reducers/deck/index.js
+++ src/store/reducers/deck/index.js
@@ -2,17 +2,14 @@ import {produce} from 'immer'
 import deck from './Stacks'
 
 export default function reducer(state=deck, action){
-    console.log('state',state)
     switch(action.type){
         case '@ADD':
-            console.log('action',action)
             return produce(state, draftState=>{
                 const {stack, cards} = action
                 const [main, deck] = stack.split('/')
                 draftState[main][deck].push(...cards)
             })
         case '@REMOVE':
-            console.log('removeu')
             return produce(state, draftState=>{
                 const {stack, index} = action
                 const [main, deck] = stack.split('/')
